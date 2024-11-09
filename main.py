1e1406049656cec9762a1a34fda015ac666b8d6f
Incept1on <incept1onnn999@gmail.com>
2024-11-09 23:02:47 +0300

    feat: now everything after / is displayed

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from collections import deque
 
 app = FastAPI()
 last_requests = deque(maxlen=10)
 
-@app.get("/")
-async def main_get(urlParams: str = Query(None)):
-    last_requests.append(urlParams)
+@app.get("/{path:path}")  
+async def main_get(path: str):
+    last_requests.append(path)
     return JSONResponse(content={"last_requests": list(last_requests)})
